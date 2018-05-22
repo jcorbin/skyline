e888b4ea8aafe60ce383d7248527e88c15acf3e0
Joshua T Corbin <jcorbin@wunjo.org>
2018-05-22 07:54:31 -0700

    Accept seed to gen

--- gen/main.go
+++ gen/main.go
@@ -14,10 +14,12 @@ func main() {
 		widthFlag  = flag.Int("w", 16, "width of field")
 		heightFlag = flag.Int("h", 32, "height of field")
 		countFlag  = flag.Int("n", 8, "number of buildings")
+		seedFlag   = flag.Int64("s", 0, "rand seed (default: random)")
 	)
 	flag.Parse()
 	if err := run(
 		os.Stdout,
+		*seedFlag,
 		*widthFlag,
 		*heightFlag,
 		*countFlag,
@@ -26,17 +28,23 @@ func main() {
 	}
 }
 
-func run(out io.Writer, w, h, n int) error {
+func run(
+	out io.Writer,
+	seed int64,
+	w, h, n int,
+) error {
+	rng := rand.New(rand.NewSource(seed))
+
 	if _, err := fmt.Fprintf(out, "x1,x2,h\n"); err != nil {
 		return err
 	}
 	for i := 0; i < n; i++ {
-		x1 := rand.Intn(w)
-		x2 := rand.Intn(w)
+		x1 := rng.Intn(w)
+		x2 := rng.Intn(w)
 		if x2 < x1 {
 			x1, x2 = x2, x1
 		}
-		h := rand.Intn(h)
+		h := rng.Intn(h)
 		if _, err := fmt.Fprintf(out, "%d,%d,%d\n", x1, x2, h); err != nil {
 			return err
 		}
