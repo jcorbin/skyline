6948e2ef735989a0aacf91711343aa4bcfceeb38
Joshua T Corbin <jcorbin@wunjo.org>
2018-05-31 09:54:07 -0400

    Factor out internal.Gen

---
+++ internal/gen.go
@@ -0,0 +1,29 @@
+package internal
+
+import (
+	"math/rand"
+)
+
+// Gen erate n random buildings chosen (rather poorly!) using the given
+// random generator and width/height constraints; calls f with each building,
+// passing through and stopping on any non-nil error.
+//
+// TODO this generator could be higher "quality"
+func Gen(
+	rng *rand.Rand,
+	w, h, n int,
+	f func(b Building) error,
+) error {
+	for i := 0; i < n; i++ {
+		x1 := rng.Intn(w)
+		x2 := rng.Intn(w)
+		if x2 < x1 {
+			x1, x2 = x2, x1
+		}
+		h := rng.Intn(h-1) + 1
+		if err := f(Building{[2]int{x1, x2}, h}); err != nil {
+			return err
+		}
+	}
+	return nil
+}
