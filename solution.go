f5c782ebe7b8a29986ed5bd81ffc6094e9cdc10a
Joshua T Corbin <jcorbin@wunjo.org>
2018-06-03 18:08:37 -0500

    Re-orient around Solver struct

--- solution.go
+++ solution.go
@@ -10,6 +10,19 @@ import (
 // Solve receives a slice of building definitions, and is expected to return
 // the correct slice of skyline-defining points.
 func Solve(data []internal.Building) ([]image.Point, error) {
+	var sol Solver
+	return sol.Solve(data)
+}
+
+// Solver holds any state for solving the skyline problem, potentially re-using
+// previously allocated state memory.
+type Solver struct {
+}
+
+// Solve receives a slice of building definitions, and is expected to return
+// the correct slice of skyline-defining points. The returned point slice is
+// only valid until the next call to Solve.
+func (sol *Solver) Solve(data []internal.Building) ([]image.Point, error) {
 	if len(data) == 0 {
 		return nil, nil
 	}
