571c3436f01635f0db94721613db49c35daf959a
Joshua T Corbin <jcorbin@wunjo.org>
2018-06-04 18:14:24 -0500

    solution: refactor to track only open end points

--- solution.go
+++ solution.go
@@ -19,9 +19,10 @@ func Solve(data []internal.Building) ([]image.Point, error) {
 type Solver struct {
 	o1  []int
 	x1  []int
-	o2  []int
 	x2  []int
 	h   []int
+	op  []int
+	rh  []int
 	res []image.Point
 }
 
@@ -35,102 +36,140 @@ func (sol *Solver) Solve(data []internal.Building) ([]image.Point, error) {
 
 	o1 := sol.o1
 	x1 := sol.x1
-	o2 := sol.o2
 	x2 := sol.x2
 	h := sol.h
+	op := sol.op
+	rh := sol.rh
 	res := sol.res
 
 	if cap(o1) < len(data) {
 		o1 = make([]int, 0, len(data))
 		x1 = make([]int, 0, len(data))
-		o2 = make([]int, 0, len(data))
 		x2 = make([]int, 0, len(data))
 		h = make([]int, 0, len(data))
+		op = make([]int, 0, len(data))
+		rh = make([]int, 0, len(data))
 		res = make([]image.Point, 0, 4*len(data))
 	} else {
 		o1 = o1[:0]
 		x1 = x1[:0]
-		o2 = o2[:0]
 		x2 = x2[:0]
 		h = h[:0]
+		op = op[:0]
+		rh = rh[:0]
 		res = res[:0]
 	}
 
 	for i := range data {
 		o1, x1 = addOrderedXPoint(o1, x1, i, data[i].Sides[0])
-		o2, x2 = addOrderedXPoint(o2, x2, i, data[i].Sides[1])
+		x2 = append(x2, data[i].Sides[1])
 		h = append(h, data[i].Height)
 	}
 
 	sol.o1 = o1
 	sol.x1 = x1
-	sol.o2 = o2
 	sol.x2 = x2
 	sol.h = h
+	sol.op = op
+	sol.rh = rh
 	sol.res = res
 
 	cur := image.ZP
-	o2i := 0
 	for o1i := 0; o1i < len(o1); o1i++ {
 		// NOTE test probably doesn't catch edge case where several co-incident
 		// opens cause redundant co-linear points
 		i := o1[o1i]
-		bx := x1[i]
-
-		// close opened past bx
-		for o2i < len(o2) && x2[o2[o2i]] < bx {
-			j := o2[o2i]
-			o2i++
-			if rh := remHeightUnder(bx, o2i, o2, x1, x2, h); rh < cur.Y {
-				cur, res = tox(cur, res, x2[j])
-				cur, res = goy(cur, res, rh)
-			}
-		}
-
-		// open data[i]
-		if bh := h[i]; bh > cur.Y {
-			cur, res = tox(cur, res, bx)
-			cur, res = goy(cur, res, bh)
-		}
+		op, rh, cur, res = closePast(i, x1, x2, op, rh, cur, res)
+		op, rh, cur, res = open(i, x1, x2, h, op, rh, cur, res)
 	}
-
-	// flush opened
-	for o2i < len(o2) {
-		j := o2[o2i]
-		o2i++
-		if rh := remHeight(o2i, o2, x1, x2, h); rh < cur.Y {
-			cur, res = tox(cur, res, x2[j])
-			cur, res = goy(cur, res, rh)
-		}
-	}
+	op, rh, cur, res = flush(x2, op, rh, cur, res)
 
 	return res, nil
 }
 
-// TODO pre-compute this rather than this expensive loop every time
-func remHeightUnder(bx, o2i int, o2, x1, x2, h []int) int {
-	rh := 0
-	for ; o2i < len(o2); o2i++ {
-		// TODO better to keep an open []int index?
-		if k := o2[o2i]; x1[k] < bx {
-			if kh := h[k]; kh > rh {
-				rh = kh
-			}
-		}
+func open(
+	i int, x1, x2, h []int,
+	op, rh []int,
+	cur image.Point, res []image.Point,
+) (
+	_, _ []int,
+	_ image.Point, _ []image.Point,
+) {
+	if bh := h[i]; bh > cur.Y {
+		cur, res = tox(cur, res, x1[i])
+		cur, res = goy(cur, res, bh)
 	}
-	return rh
+	op, rh = appendRH(i, x2, h, op, rh)
+	return op, rh, cur, res
 }
 
-// TODO pre-compute this rather than this expensive loop every time
-func remHeight(o2i int, o2, x1, x2, h []int) int {
-	rh := 0
-	for ; o2i < len(o2); o2i++ {
-		// TODO better to keep an open []int index?
-		if kh := h[o2[o2i]]; kh > rh {
-			rh = kh
+func appendRH(i int, x2, h, op, rh []int) (_, _ []int) {
+	nop := len(op)
+	opi := sort.Search(nop, func(opi int) bool { return x2[op[opi]] > x2[i] })
+	op, rh = append(op, i), append(rh, 0)
+
+	if opi != nop {
+		copy(op[opi+1:], op[opi:])
+		op[opi] = i
+	}
+
+	// TODO should be able to do this with a partial update
+	mh := 0
+	for opi := len(op) - 1; opi > 0; {
+		if oh := h[op[opi]]; mh < oh {
+			mh = oh
+		}
+		opi--
+		rh[opi] = mh
+	}
+
+	return op, rh
+}
+
+func closePast(
+	i int, x1, x2 []int,
+	op, rh []int,
+	cur image.Point, res []image.Point,
+) (
+	_, _ []int,
+	_ image.Point, _ []image.Point,
+) {
+	bx := x1[i]
+	opi := 0
+	for ; opi < len(op); opi++ {
+		j := op[opi]
+		if x2[j] >= bx {
+			break
+		}
+		if ah := rh[opi]; ah < cur.Y {
+			cur, res = tox(cur, res, x2[j])
+			cur, res = goy(cur, res, ah)
 		}
 	}
-	return rh
+	op = op[:copy(op, op[opi:])]
+	rh = rh[:copy(rh, rh[opi:])]
+	return op, rh, cur, res
+}
+
+func flush(
+	x2 []int,
+	op, rh []int,
+	cur image.Point, res []image.Point,
+) (
+	_, _ []int,
+	_ image.Point, _ []image.Point,
+) {
+	opi := 0
+	for ; opi < len(op); opi++ {
+		j := op[opi]
+		if ah := rh[opi]; ah < cur.Y {
+			cur, res = tox(cur, res, x2[j])
+			cur, res = goy(cur, res, ah)
+		}
+	}
+	op = op[:0]
+	rh = rh[:0]
+	return op, rh, cur, res
 }
 
 func addOrderedXPoint(os, xs []int, i, x int) (_, _ []int) {
